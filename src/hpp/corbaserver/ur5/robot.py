8e0c745032621f6bbe9e37e5c5500ef8f3974cc3
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-04-05 14:38:23 +0200

    format

--- src/hpp/corbaserver/ur5/robot.py
+++ src/hpp/corbaserver/ur5/robot.py
@@ -18,16 +18,19 @@
 
 from hpp.corbaserver.robot import Robot as Parent
 
-class Robot (Parent):
-    urdfFilename = "package://example-robot-data/robots/ur_description/urdf/ur5_joint_limited_robot.urdf"
-    srdfFilename = "package://example-robot-data/robots/ur_description/srdf/ur5_joint_limited_robot.srdf"
+PACKAGE = "package://example-robot-data/robots/ur_description/"
 
-    def __init__ (self, robotName, load = True, rootJointType = "anchor"):
-        Parent.__init__ (self, robotName, rootJointType, load)
+
+class Robot(Parent):
+    urdfFilename = PACKAGE + "urdf/ur5_joint_limited_robot.urdf"
+    srdfFilename = PACKAGE + "srdf/ur5_joint_limited_robot.srdf"
+
+    def __init__(self, robotName, load=True, rootJointType="anchor"):
+        Parent.__init__(self, robotName, rootJointType, load)
         self.rightWrist = "wrist_3_joint"
-        self.leftWrist  = "wrist_3_joint"
+        self.leftWrist = "wrist_3_joint"
         self.endEffector = "ee_fixed_joint"
 
-    def getInitialConfig (self):
-        q = 6*[0]
+    def getInitialConfig(self):
+        q = 6 * [0]
         return q
