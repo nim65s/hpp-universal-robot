5123cb18e35c5cf5b03b0ea19f36a7b66c5f08a8
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-07 14:27:46 +0200

    nix: move package to nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,8 @@
 {
   description = "Data specific to robots ur5 and ur10 for hpp-corbaserver";
 
-  nixConfig = {
-    extra-substituters = [ "https://gepetto.cachix.org" ];
-    extra-trusted-public-keys = [ "gepetto.cachix.org-1:toswMl31VewC0jGkN6+gOelO2Yom0SOHzPwJMY2XiDY=" ];
-  };
-
   inputs = {
-    nixpkgs.url = "github:nim65s/nixpkgs/gepetto";
+    nixpkgs.url = "github:gepetto/nixpkgs";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -15,9 +10,8 @@
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ];
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -25,18 +19,23 @@
         "x86_64-darwin"
       ];
       perSystem =
+        { pkgs, self', ... }:
         {
-          self',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          packages = {
-            inherit (pkgs) cachix;
-            default = pkgs.callPackage ./. { };
-          };
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          packages = {
+            default = self'.packages.hpp-universal-robot;
+            hpp-universal-robot = pkgs.python3Packages.hpp-universal-robot.overrideAttrs (_: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./doc
+                  ./package.xml
+                  ./src
+                ];
+              };
+            });
+          };
         };
     };
 }
