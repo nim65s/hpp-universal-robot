8dd045e5fd6041eac3119ae1d57ee62d3513b150
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 11:25:02 +0200

    fix v8.0.0 merge

--- flake.nix
+++ flake.nix
@@ -1,38 +1,24 @@
 {
   description = "Data specific to robots ur5 and ur10 for hpp-corbaserver";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            gazebros2nix.overrides.hpp-universal-robot = _final: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./package.xml
-                  ./src
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-universal-robot = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./package.xml
+              ./src
+            ];
+          };
+        };
       }
     );
 }
